6177f98fc4e687884e7067483af5a7b990a21151
feellliiiisssss <94290769+feellliiiisssss@users.noreply.github.com>
2022-05-18 22:03:38 +0700

    Add pop up for quick view

--- assets/js/main.js
+++ assets/js/main.js
@@ -277,4 +277,28 @@
       mirror: false,
     });
   });
+
+  /*Quick Review*/
+  let preveiwContainer = document.querySelector(".products-preview");
+  let previewBox = preveiwContainer.querySelectorAll(".preview");
+
+  document.querySelectorAll(".product-grid5 .select-options").forEach((product) => {
+    product.onclick = () => {
+      preveiwContainer.style.display = "flex";
+      let name = product.getAttribute("data-name");
+      previewBox.forEach((preview) => {
+        let target = preview.getAttribute("data-target");
+        if (name == target) {
+          preview.classList.add("active");
+        }
+      });
+    };
+  });
+
+  previewBox.forEach((close) => {
+    close.querySelector(".fa-times").onclick = () => {
+      close.classList.remove("active");
+      preveiwContainer.style.display = "none";
+    };
+  });
 })();
