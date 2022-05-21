b23bb6f567ec752007a84ae82da55165cbea6839
Edwin Sulaiman <edwinsulaiman20@gmail.com>
2022-05-21 20:31:40 +0700

    Initialize database and register route

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
 
-session_start();
-session_destroy();
+// Initalize database.
+require_once "db.class.php";
+
+DB::$user = "root";
+DB::$password = "";
+DB::$dbName = "guitar_shop";
+
+// Register routes.
+require_once "routes.php";
